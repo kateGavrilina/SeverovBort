c2f468544113a9072ed7d66033b85a30d9762a84
klyamina <gagarin095@gmail.com>
2022-08-30 14:56:13 +0300

    Добавлен прототип бортовой СУ, в которую включены: - обмен с VectorNav; - обмен с kx-pult; - обмен с пультом.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <QCoreApplication>
+#include "vectornavprotocol.h"
+#include <QThread>
+#include "kx_pult/kx_protocol.h"
+#include "kx_pult/qkx_coeffs.h"
+#include "pultcontrolsystemprotocols.h"
+
+const QString ConfigFile = "protocols.conf";
+const QString XI = "xi";
+const QString KI = "ki";
+
+double X[2000][2];
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    //передача K
+    Qkx_coeffs * kProtocol = new Qkx_coeffs(ConfigFile, KI);
+    //передача X
+    x_protocol * xProtocol = new x_protocol(ConfigFile,XI,X);
+    //обмен с VectorNav (там внутри пример с примененем X-ов)
+    VectorNavProtocol vn100Proto;
+    //обмен с пультом
+    ControlSystem::PC_Protocol *pultProtocol = new ControlSystem::PC_Protocol(ConfigFile,"rov_pult");
+    return a.exec();
+}
---
+++ vectornavprotocol.h
@@ -0,0 +1,64 @@
+#ifndef VECTORNAVPROTOCOL_H
+#define VECTORNAVPROTOCOL_H
+
+#include <QObject>
+#include <QTimer>
+#include <stdio.h>
+#include <unistd.h>
+#include "include_vn100/vectornav.h"
+#include <QThread>
+#include <QDebug>
+
+const char* const COM_PORT = "COM5";
+const int BAUD_RATE = 115200;
+extern double X[2000][2];
+
+class VectorNavProtocol : public QObject
+{
+    Q_OBJECT
+public:
+    VectorNavProtocol(QObject * prnt = nullptr):QObject(prnt){
+        QThread * thread = new QThread();
+        moveToThread(thread);
+        thread->start();
+        vn100_connect(&vn100, COM_PORT, BAUD_RATE);
+        Sleep(1000);
+        connect(&timer, &QTimer::timeout, this, &VectorNavProtocol::tick);
+    }
+    virtual ~VectorNavProtocol(){
+        vn100_disconnect(&vn100);
+    }
+public slots:
+    void start (int dt){
+        timer.start(dt);
+    }
+    void stop(){
+        timer.stop();
+    }
+    void tick(){
+       //Vn100CompositeData data;
+       //vn100_getCurrentAsyncData(&vn100, &data);
+       vn100_getYawPitchRollTrueInertialAcclerationAngularRate(&vn100,&ypr,&inertialAcceleration,&angularRate);
+       //vn100_getCalibratedImuMeasurements(&vn100,&magnetic,&inertialAcceleration,&angularRate, &temp);
+
+        X[301][0] = ypr.yaw;
+        X[302][0] = ypr.pitch;
+        X[303][0] = ypr.roll;
+        X[304][0] = inertialAcceleration.c0;
+        X[305][0] = inertialAcceleration.c1;
+        X[306][0] = inertialAcceleration.c2;
+        X[307][0] = angularRate.c0;
+        X[308][0] = angularRate.c1;
+        X[309][0] = angularRate.c2;
+    }
+protected:
+    QTimer timer;
+    Vn100 vn100;
+    VnYpr ypr;
+    VnVector3 inertialAcceleration;
+    VnVector3 angularRate;
+    VnVector3 magnetic;
+    double temp;
+};
+
+#endif // VECTORNAVPROTOCOL_H
